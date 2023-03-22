2825d9920ed86dbd0b5a80fac9e2fe576575f502
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2023-03-22 02:26:42 -0600

    calendario provisional de dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,11 +4,12 @@
         </h2>
     </x-slot> --}}
 
-    <div class="py-12">
+    {{-- <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
 
             </div>
         </div>
-    </div>
+    </div> --}}
+    <x-helpers.calendar />
 </x-app-layout>
